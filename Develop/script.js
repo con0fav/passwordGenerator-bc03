021dc603bc226cb1aac05ed74db4baa1766a2870
con0fav <connor.favero@gmail.com>
2020-11-05 15:45:22 -0800

    adding functionality code and variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,7 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+
+
 
 
 //be sure to console.log
@@ -7,44 +9,55 @@ var generateBtn = document.querySelector("#generate");
 // to build array use var lowChar ="abc" ".split"("");
 //^also google split 
 
+//radio buttons assigned to toggle each function
 
+function getRandomUpperCase(){
+  return String.fromCharCode(math.floor(math.random()*26)+65);
+}
 
-//ex: var numbers = [0,1,2,3,4,5,6,7,9]
+function getRandomLowerCase(){
+  return String.fromCharCode(math.floor(math.random()*26)+97);
+}
 
-//array list of lowercase
+function getRandomNumber(){
+  return String.fromCharCode(math.floor(math.random()*26)+10);
+}
 
-//array list of uppercase
-//lowercase to uppercase?
+function getRandomSpecial(){
+  var special = "!@#$%^&*()[]{}<>,-~?"
+  return special[math.floor(math.random()*special.length)];
+}
 
-//array list of special characters
+//potentially turn functions into variables?
+//figure out if math.random has to be used to return random values
 
-//to goog:
-//return string from charCode random number
-//radio buttons (style of button) boolean button, in the html
-//document.query selector
-//switch cases
-//dot value, how it manipulates dom
-//how to copy password element after gweneration
-//how to save password in local storage
+var generatePass = {
+  upper : getRandomUpperCase,
+  lower : getRandomLowerCase,
+  number : getRandomNumber,
+  special : getRandomSpecial
+};
 
+//console.log(generatePass);
+//ex: var numbers = [0,1,2,3,4,5,6,7,9] for arrays, lower, upper, special
 
+//asks for what values in password
 function generatePassword() {
-
-
+  confirm(getRandomLowerCase);
+  confirm(getRandomUpperCase);
+  confirm(getRandomNumber);
+  confirm(getRandomSpecial);
 
 }
 
+//console.log(generatePassword)
 //password length
 //const passwordLength = prompt()
 //confirm use number
-
 //confirm use lowercase
-
 //confirm use uppercase
-
 //confirm use special
-
-// Write password to the #password input
+// Write password to the #password input, using prompt()
 
 function writePassword() {
   var password = generatePassword();
@@ -54,5 +67,14 @@ function writePassword() {
 
 }
 
+
+//pushes password into text area
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//to do for enhanced functionality:
+//radio buttons (style of button) boolean button, in the html
+//how to copy password element after generation
+//how to save password in local storage
