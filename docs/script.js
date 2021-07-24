2c68241f65fe9e7afb276d8458b28623545c337e
con0fav <connor.favero@gmail.com>
2021-07-23 17:27:34 -0700

    testing copypassword function

--- docs/script.js
+++ docs/script.js
@@ -54,7 +54,14 @@ function writePassword() {
 }
 
 function copyPassword(){
-  
+  var copyText = document.getElementById("#password");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+
+  document.execCommand("copy");
+
+  alert("added password to clipboard");
 }
 
 
